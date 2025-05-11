d41a8f2c17ce7fe7e1ac9973682f9bb401d4d0da
bfarley15 <166653082+bfarley15@users.noreply.github.com>
2025-05-11 12:02:56 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,38 @@
-self.addEventListener("install", e => {
-    console.log("Service Worker Installed");
-  });
-  
-  self.addEventListener("fetch", e => {
-    e.respondWith(fetch(e.request));
-  });
-  
+const CACHE_NAME = 'scorekeeper-v1';
+const URLS_TO_CACHE = [
+  'index.html',
+  'manifest.json',
+  'icon-192.png',
+  'icon-512.png'
+];
+
+// Install event
+self.addEventListener('install', event => {
+  console.log('Service Worker: Installing');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+  console.log('Service Worker: Activated');
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
